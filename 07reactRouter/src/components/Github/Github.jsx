a7ef9e19ac58b153e203813db5f73fa69157b1aa
chetanbedse <chetanbedse5041@gmail.com>
2023-10-22 00:07:43 +0530

    Github Component

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -67,10 +67,10 @@ function Github() {
               </div>
               <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                 <dt className="text-sm font-medium leading-6 text-gray-900">
-                  Github Followers
+                  Public Repositories
                 </dt>
                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  {data.followers}
+                  {data.public_repos}
                 </dd>
               </div>
             </dl>
